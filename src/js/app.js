902f8432b7181637fe81ace06a53bea39a7248b9
alchimic <alchimicus@gmail.com>
2020-05-05 03:20:49 +0300

    start project factor

---
+++ src/js/app.js
@@ -0,0 +1,16 @@
+// import sayHello from './lib/sayHello.js';
+
+$(document).ready(function() {
+
+  $('.hero__slider').slick({
+
+    dots: true,
+    arrow: false,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    cssEase: 'linear',
+    infinite: true
+  });
+
+
+});
